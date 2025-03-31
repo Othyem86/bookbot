735156d859ce89a4e9d55a7a2ae3c2c729d191a8
Othyem86 <emil.foeger@gmail.com>
2025-03-31 20:25:20 +0200

    added pretty group report

--- main.py
+++ main.py
@@ -4,14 +4,30 @@ def main() -> None:
     book_file_path = "./books/frankenstein.txt"
     text = get_book_text(book_file_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
     num_chars = get_num_characters(text)
-    print(num_chars)
+    print_report(book_file_path, num_words, num_chars)
 
 
 def get_book_text(file_path: str) -> str:
     with open(file_path) as file:
         return file.read()
+
+
+def print_report(
+        book_path: str,
+        num_words: int,
+        chars_sorted_list: dict[str, int]) -> None:
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    sorted_chars = char_dict_to_sorted_list(chars_sorted_list)
+    for item in sorted_chars:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item["char"]}: {item["num"]}")
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,13 @@ def get_num_characters(text: str) -> dict[str, int]:
             continue
         num_characters[char] += 1
     return num_characters
+
+
+
+
+def char_dict_to_sorted_list(num_chars_dict: dict[str, int]) -> list[dict[str, int]]:
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=lambda x: x["num"])
+    return sorted_list
