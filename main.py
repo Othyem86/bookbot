349bf024615e8533458eabed4bbe874d516cc537
Othyem86 <emil.foeger@gmail.com>
2025-04-01 10:23:10 +0200

    added sys.argv and os methods to flow control

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+import sys
+import os
 from stats import *
 
+
 def main() -> None:
-    book_file_path = "./books/frankenstein.txt"
-    text = get_book_text(book_file_path)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
+    if (not os.path.exists(file_path)):
+        print(f"ERROR: File {file_path} could not be found")
+        sys.exit(1)
+    text = get_book_text(file_path)
     num_words = get_num_words(text)
     num_chars = get_num_characters(text)
-    print_report(book_file_path, num_words, num_chars)
+    print_report(file_path, num_words, num_chars)
+    sys.exit(0)
 
 
 def get_book_text(file_path: str) -> str:
